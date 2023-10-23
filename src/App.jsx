b37969649c37af6e106a81d98aed71d37b9ede53
M0hanad1 <mohned.dabsha@gmail.com>
2023-10-23 21:38:26 +0200

    Moved all header files to Header/ folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
