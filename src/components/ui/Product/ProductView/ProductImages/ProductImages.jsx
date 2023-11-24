cd3c13a25b0619792f965a7ecbb653c403735476
M0hanad1 <mohned.dabsha@gmail.com>
2023-11-24 14:48:23 +0200

    You can now choose an image in ProductView

--- src/components/ui/Product/ProductView/ProductImages/ProductImages.jsx
+++ src/components/ui/Product/ProductView/ProductImages/ProductImages.jsx
@@ -2,8 +2,10 @@ import Loader from "../../../../Loaders/Loader";
 import ImageLoader from "../../../../Loaders/ImageLoader";
 import Thumbnail from "../../Thumbnail/Thumbnail";
 import "./ProductImages.css";
+import { useState } from "react";
 
 export default function ProductImages(props) {
+    const [active, setActive] = useState();
     return (
         <div className="product-images">
             {!Object.keys(props).length ? (
@@ -11,11 +13,35 @@ export default function ProductImages(props) {
             ) : (
                 <>
                     <div className="holder">
-                        {props.images.map((value, index) => (
-                            <ImageLoader src={value} key={index} />
-                        ))}
+                        <button
+                            className={
+                                !active || active === props.thumbnail
+                                    ? "active"
+                                    : ""
+                            }
+                            onClick={() => setActive(props.thumbnail)}
+                        >
+                            <ImageLoader src={props.thumbnail} />
+                        </button>
+                        {props.images.map(
+                            (value, index) =>
+                                value !== props.thumbnail && (
+                                    <button
+                                        className={
+                                            active === value ? "active" : ""
+                                        }
+                                        key={index}
+                                        onClick={() => setActive(value)}
+                                    >
+                                        <ImageLoader src={value} />
+                                    </button>
+                                )
+                        )}
                     </div>
-                    <Thumbnail {...props} />
+                    <Thumbnail
+                        {...props}
+                        thumbnail={active || props.thumbnail}
+                    />
                 </>
             )}
         </div>
