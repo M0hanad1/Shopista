1994d47de51c71e062a3773e47e0afb39ae07769
M0hanad1 <mohned.dabsha@gmail.com>
2023-12-02 22:23:58 +0200

    Fixing react router, add dist to .gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "assets/normalize.css";
 import "assets/fontawesome.min.css";
 import "assets/solid.min.css";
@@ -8,7 +8,7 @@ import "assets/regular.min.css";
 import "assets/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
